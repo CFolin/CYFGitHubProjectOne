b0b4fb8f36284531958cf54a288897c4ffca3366
fu.colin <fucolin@fucolindemacbook-pro.local>
2016-04-24 16:09:56 +0800

    初始化项目

---
+++ GitHubOne/GitHubOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubOne
+//
+//  Created by fu.colin on 16/4/24.
+//  Copyright © 2016年 fu.colin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubOne/GitHubOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubOne
+//
+//  Created by fu.colin on 16/4/24.
+//  Copyright © 2016年 fu.colin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
